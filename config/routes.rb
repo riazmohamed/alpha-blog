48a031b057dc57ee64ae2ca1011059c8c84fa20b
Riaz Mohamed <railsdevriaz@gmail.com>
2016-07-23 12:32:51 +0000

    Create new, show, index and edit template. Create action and route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   
+  resources :articles
+  
   
   # get 'pages/home', to: 'pages#home'
   # get 'pages/about', to: 'pages#about'
