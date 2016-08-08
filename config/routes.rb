605c5deaaaa1cffa2bbb4c44b525c0753ac11ff2
Riaz Mohamed <railsdevriaz@gmail.com>
2016-08-08 10:43:24 +0000

    Add Login functionality for users

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   
   resources :users, except: [:new]
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#ndestroy'
   
   # get 'pages/home', to: 'pages#home'
   # get 'pages/about', to: 'pages#about'
